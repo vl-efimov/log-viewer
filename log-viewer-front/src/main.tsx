e20cf34265c6fa391c52d900965a6536c3e28660
Vladimir Efimov <vae.94@mail.ru>
2024-11-16 14:35:54 +0100

    Full application caching

--- log-viewer-front/src/main.tsx
+++ log-viewer-front/src/main.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
--- log-viewer-front/vite.config.ts
+++ log-viewer-front/vite.config.ts
@@ -6,18 +6,47 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      devOptions: {
-        enabled: true,
-      },
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'service-worker.ts',
-      registerType: 'autoUpdate',
-      injectManifest: {
-        swDest: 'dist/service-worker.js'
-      },
-      includeAssets: ['favicon.svg', 'robots.txt'],
-      manifestFilename: './manifest.json',
+        workbox: {
+            globPatterns: ["**/*"],
+        },
+        includeAssets: [
+            "**/*",
+        ],
+        manifest: {
+          "name": "My PWA App",
+          "short_name": "PWA App",
+          "description": "A simple PWA built with React, TypeScript, and Vite",
+          "start_url": ".",
+          "display": "standalone",
+          "background_color": "#ffffff",
+          "theme_color": "#ffffff",
+          "icons": [
+            {
+              "src": "/pwa-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png"
+            },
+            {
+              "src": "/pwa-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png"
+            }
+          ],
+          "screenshots": [
+              {
+                "src": "/screenshot-wide-desktop.png",
+                "sizes": "2560x1440",
+                "type": "image/png",
+                "form_factor": "wide"
+              },
+              {
+                "src": "/screenshot-wide-mobile.png",
+                "sizes": "850x1440",
+                "type": "image/png",
+                "form_factor": "narrow"
+              }
+          ]
+        },
     }),
   ],
 });
