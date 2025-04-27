21a72ccffe69e294c267899ab4790934762dbf47
Vladimir Efimov <vae.94@mail.ru>
2025-04-27 16:47:51 +0200

    Added in localStorage theme info

--- log-viewer-front/src/components/ThemeProvider.tsx
+++ log-viewer-front/src/components/ThemeProvider.tsx
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { ColorModeEnum } from '../constants/ColorModeEnum';
 import { darkTheme, lightTheme } from '../theme';
 
-export function ThemeProvider ({ children }: { children: React.ReactNode }) {
-    const [mode, setMode] = useState<ColorModeEnum>(ColorModeEnum.Dark);
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [mode, setMode] = useState<ColorModeEnum | null>(null);
+
+    useEffect(() => {
+        const storedMode = localStorage.getItem('theme');
+        const initialMode = storedMode ? (storedMode as ColorModeEnum) : ColorModeEnum.Dark;
+
+        setMode(initialMode);
+    }, []);
+
+    if (mode === null) {
+        return <div />;
+    }
 
     const toggleTheme = () => {
-        setMode((prev) => (prev === ColorModeEnum.Light ? ColorModeEnum.Dark : ColorModeEnum.Light));
+        const newMode = mode === ColorModeEnum.Light ? ColorModeEnum.Dark : ColorModeEnum.Light;
+        setMode(newMode);
+
+        localStorage.setItem('theme', newMode);
     };
 
     return (
