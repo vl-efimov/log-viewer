598ec0cc3cd559cbda596eaf37cbf22ac787d1bf
Vladimir <vae.94@mail.ru>
2026-03-29 21:12:20 +0200

    Deleted all backend files

--- backend/app/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY", "my_super_secret_key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
